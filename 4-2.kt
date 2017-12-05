8084aea294110fdc1d111ad31df1ccb0eb16265a
Mikko Esko <mikko.esko@student.tut.fi>
2017-12-05 10:19:22 +0200

    Added fourth day

---
+++ 4-2.kt
@@ -0,0 +1,18 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputfile = File("4-input.txt")
+    val text:List<String> = inputfile.bufferedReader().readLines()
+
+    var valids = 0
+    var total = 0
+    for (line in text) {
+        total += 1
+        val splitted_line = line.split(" ")
+        val arranged = splitted_line.map { s -> s.split("").sorted().joinToString() }
+        if (arranged.distinct().size == arranged.size) {
+            valids += 1
+        }
+    }
+    println("$valids/$total passphrases are valid")
+}
