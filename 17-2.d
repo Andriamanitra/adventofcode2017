64f71278c8fb5ed22cdf40ab99bb78eb5191852b
Andriamanitra <mikko77792@gmail.com>
2017-12-17 11:08:06 +0200

    Tweaked the code so we don't need to keep track of the index of 0

--- 17-2.d
+++ 17-2.d
@@ -9,17 +9,14 @@ void main(string[] args) {
     writefln("Using step size %d", step);
     
     int until = 50_000_000;
-    int index_of_zero = 0;
     int after_zero = 1;
     int length_of_array = 2;
     int curr = 1;
     for (int to_add = 2; to_add <= until; to_add++) {
-        curr = (curr + step + 1) % length_of_array;
-        if (curr == index_of_zero+1) {
+        // 0 always stays at first index
+        curr = (curr + step) % length_of_array + 1;
+        if (curr == 1) {
             after_zero = to_add;
-        }
-        if (curr <= index_of_zero) {
-            index_of_zero++;
         }
         length_of_array++;
     };
